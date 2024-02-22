52b9fb43ed805b6b3eedd1858ff5545a4dd3aaa6
Sodrul27 <sodrulahmad27@gmail.com>
2024-02-22 12:06:55 +0700

    Penggunaan Constructor, Memanggil constructor didalam constructor, Membuat object dari class Person menggunakan Constructor

--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,23 @@ public class Person {
     String address;
     final String country = "Indonesia";
 
+//    Constructor Default
+    Person(){
+
+    }
+
+//    Constructor dengan 1 Parameter
+    Person(String paramName){
+        name = paramName;
+    }
+
+//    Constructor dengan 2 parameter
+    Person(String paramName, String paramAddress){
+//        Memanggil constructor didalam constructor pertama
+        this(paramName);
+        address = paramAddress;
+    }
+
     void sayHello(String paramName){
         System.out.println("Hello " + paramName + ", My name is " + name + ".");
     }
