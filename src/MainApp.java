b8afac742fd9fa944bf5a05ad824947a3b8ae083
Sodrul27 <sodrulahmad27@gmail.com>
2024-02-22 11:33:19 +0700

    Menambahkan Class Person, Mengakses Class Person, Memanipulasi Field

---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+public class MainApp {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Ahmad";
+        person1.address = "Tangerang";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+    }
+}
