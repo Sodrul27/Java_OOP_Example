74f3c8b195d1f4b92a8671fbb074a6304cf7f32c
Sodrul27 <sodrulahmad27@gmail.com>
2024-02-25 19:36:50 +0700

    Pemanggilan Objek menggunakan Polymorphism

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,58 +2,46 @@ import javax.print.Doc;
 
 public class MainApp {
     public static void main(String[] args) {
-//        Membuat Objek Dari class Parent Person
-//        Menggunakan cunstructor default
-        Person person1 = new Person();
-        person1.name = "Ahmad";
-        person1.address = "Tangerang";
 
-//        Membuat Objek Dari class child Teacher
-//        Menggunakan cunstructor default
-        Teacher teacher1 = new Teacher();
-        teacher1.name = "Sodrul";
-        teacher1.address = "Karang Tengah";
-        teacher1.subject = "Java Programming";
 
-//        Menggunakan cunstructor super
-        Teacher teacher2 = new Teacher("Hisyam", "Tangerang", "Multimedia");
+        Person person1 = new Programmer("Ahmad", "Tangerang", "Java"); // Object 1
+        /*
+        bila ingin mengambil field pada child class tapi menggunakan type data
+        parent class maka perlu dilakukan Type check & Casts seperti dibawah.
+        */
+        System.out.println(((Programmer)person1).technology);
 
-//        Membuat Objek Dari class child Doctor
-//        Menggunakan cunstructor default
-        Doctor doctor1 = new Doctor();
-        doctor1.name = "Tamam";
-        doctor1.address = "Jakarta";
-        doctor1.specialist = "Dentis";
 
-//        Menggunakan cunstructor super
-        Doctor doctor2 = new Doctor("Mufa", "Demak", "Bedah");
+        Person person2 = new Teacher("Mufa", "Demak", "B.Inggris"); // Object 2
+        Person person3 = new Doctor("Sodrul", "Parung", "Bedah"); // Object 3
 
-//        Membuat Objek Dari class child Programmer
-//        Menggunakan cunstructor default
-        Programmer programmer1 = new Programmer();
-        programmer1.name = "Odru";
-        programmer1.address = "Demak";
-        programmer1.technology = "Java";
+        sayHello(person1);
+        sayHello(person2);
+        sayHello(person3);
 
-//        Menggunakan cunstructor super
-        Programmer programmer2 = new Programmer("Rika", "Surabaya", "Javascript");
+    }
 
-//        Implementasi method parent class
-        person1.greeting();
-        System.out.println();
-
-        teacher1.greeting();
-        System.out.println("====================");
-        teacher2.greeting();
-        System.out.println();
-
-        doctor1.greeting();
-        System.out.println("====================");
-        doctor2.greeting();
-        System.out.println();
-
-        programmer1.greeting();
-        System.out.println("====================");
-        programmer2.greeting();
+    /* Metod sayHello
+        yang didalamnya terdapat pengecekan, dengan kondisi melakukan pengecekan
+        tipe data atau lebih tepatnya pengecekan instantiation apa yang digunakan
+        pembuatan object.
+        Dengan menggunakan instanceof, apakah object tersebut di instansiasi oleh
+        masing-masing class
+     */
+    static void sayHello(Person person){
+        String message;
+        if(person instanceof Programmer){
+            Programmer programmer = (Programmer) person;
+            message = "Hello " + programmer.name + ". Seorang Programmer " + programmer.technology + ".";
+        } else if (person instanceof Teacher) {
+            Teacher teacher = (Teacher) person;
+            message = "Hello " + teacher.name + ". Seorang Guru " + teacher.subject + ".";
+        } else if (person instanceof Doctor) {
+            Doctor doctor = (Doctor) person;
+            message = "Hello " + doctor.name + ". Seorang Docter " + doctor.specialist + ".";
+        } else {
+            message = "Hello " + person.name + ".";
+        }
+        System.out.println(message);
     }
 }
