db7e575b4ec6ad804cd0cf8c8fcd5658e63a6447
Sodrul27 <sodrulahmad27@gmail.com>
2024-02-26 20:29:16 +0700

    Penggunaan Abstract Method

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,9 +1,18 @@
-import child.Item;
-import parent.Product;
-
+import child.Circle;
+import child.Triangle;
+import parent.Shape;
 
 public class MainApp {
     public static void main(String[] args) {
-        Product product1 = new Item();
+//        menggunakan tipe yang sama tetapi di instansiasi menggunakan child class yang berbeda
+        Shape lingkarang = new Circle(20, "Blue");
+        Shape segitiga = new Triangle(10,20, "Red");
+
+        System.out.println("Luas lingkaran berwarna " + lingkarang.getColor() + " adalah "
+                + lingkarang.getArea());
+        System.out.println("Luas Segitiga berwarna " + segitiga.getColor() + " adalah "
+         + segitiga.getArea());
+
+
     }
 }
---
+++ src/parent/Shape.java
@@ -0,0 +1,17 @@
+package parent;
+public abstract class Shape {
+    private String color;
+    public Shape(){
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract double getArea();
+}
