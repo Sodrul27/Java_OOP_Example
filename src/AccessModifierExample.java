e2b0d79da1b812431c21edb8c676d2ddb43d62d0
Sodrul27 <sodrulahmad27@gmail.com>
2024-02-26 15:37:29 +0700

    Access Modifier default dan public

---
+++ src/AccessModifierExample.java
@@ -0,0 +1,19 @@
+public class AccessModifierExample {
+//    Access Modifier default
+    String name;
+    String address;
+
+//    contoh method menggunakan Modifier default
+    void sayHello(){
+
+    }
+
+//    Access MOdifier Public
+    public String nama;
+    public String alamat;
+
+//    contoh method menggunakan modifier public
+    public void ucapan(){
+
+    }
+}
